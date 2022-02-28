420bcf23b961109ef6d01ce514472e4d5097859c
Safi <safikurrahman7514@gmail.com>
2022-03-01 00:55:02 +0600

    added event on search button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+// search button add event
+document.getElementById('search-btn').addEventListener('click', function () {
+    const searchField = document.getElementById('search-field');
+    // console.log(searchField)
+    const searchValue = searchField.value;
+    searchField.value = "";
+    // console.log(searchValue)
 
+})
